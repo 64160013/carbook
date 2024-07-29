cc22723aa95b32e1d3ca99f18f2acbc6a1690e50
64160013 <64160013@go.buu.ac.th>
2024-07-29 20:31:19 +0700

    CalenderUpNoneconfig

--- resources/views/fullcalender.blade.php
+++ resources/views/fullcalender.blade.php
@@ -2,19 +2,21 @@
 <html>
 <head>
 <meta charset='utf-8' />
-<script src='../dist/index.global.js'></script>
+<link href='https://unpkg.com/fullcalendar@5.11.3/main.min.css' rel='stylesheet' />
+<script src='https://unpkg.com/fullcalendar@5.11.3/main.min.js'></script>
 <script>
-
   document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
+    var today = new Date().toISOString().slice(0, 10); // ดึงวันที่ในรูปแบบ 'YYYY-MM-DD'
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
       headerToolbar: {
         left: 'prev,next today',
         center: 'title',
-        right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+        right: 'dayGridMonth,listMonth'
+        //right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
       },
-      initialDate: '2023-01-12',
+      initialDate: today, // ใช้วันที่ปัจจุบันเป็นค่าเริ่มต้น
       navLinks: true, // can click day/week names to navigate views
       businessHours: true, // display business hours
       editable: true,
@@ -22,60 +24,64 @@
       events: [
         {
           title: 'Business Lunch',
-          start: '2023-01-03T13:00:00',
+          start: '2024-07-03T13:00:00',
           constraint: 'businessHours'
         },
         {
           title: 'Meeting',
-          start: '2023-01-13T11:00:00',
+          start: '2024-07-13T11:00:00',
           constraint: 'availableForMeeting', // defined below
           color: '#257e4a'
         },
         {
           title: 'Conference',
-          start: '2023-01-18',
-          end: '2023-01-20'
+          start: '2024-07-18',
+          end: '2024-07-20'
+        },
+        {
+          title: 'test',
+          start: '2024-07-18',
+          end: '2024-07-18'
         },
         {
           title: 'Party',
-          start: '2023-01-29T20:00:00'
+          start: '2024-07-29T20:00:00'
         },
 
         // areas where "Meeting" must be dropped
         {
           groupId: 'availableForMeeting',
-          start: '2023-01-11T10:00:00',
-          end: '2023-01-11T16:00:00',
-          display: 'background'
+          start: '2024-07-11T10:00:00',
+          end: '2024-07-11T16:00:00',
+          display: 'ff9f89'   //เปลี่ยนจาก bg->rgb แล้วงง
         },
         {
           groupId: 'availableForMeeting',
-          start: '2023-01-13T10:00:00',
-          end: '2023-01-13T16:00:00',
+          start: '2024-07-13T10:00:00',
+          end: '2024-07-13T16:00:00',
           display: 'background'
         },
 
         // red areas where no events can be dropped
         {
-          start: '2023-01-24',
-          end: '2023-01-28',
+          start: '2024-07-24',
+          end: '2024-07-28',
           overlap: false,
           display: 'background',
           color: '#ff9f89'
         },
         {
-          start: '2023-01-06',
-          end: '2023-01-08',
+          start: '2024-07-06',
+          end: '2024-07-09',   //ไม่ตรงกับหน้า display
           overlap: false,
           display: 'background',
-          color: '#ff9f89'
+          color: '#000'
         }
       ]
     });
 
     calendar.render();
   });
-
 </script>
 <style>
 
@@ -99,107 +105,3 @@
 
 </body>
 </html>
-
-
-
-<!-- <!DOCTYPE html>
-<html>
-<head>
-<meta charset='utf-8' />
-<script src='../dist/index.global.js'></script>
-<script>
-
-  document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      headerToolbar: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
-      },
-      initialDate: '2023-01-12',
-      navLinks: true, // can click day/week names to navigate views
-      businessHours: true, // display business hours
-      editable: true,
-      selectable: true,
-      events: [
-        {
-          title: 'Business Lunch',
-          start: '2023-01-03T13:00:00',
-          constraint: 'businessHours'
-        },
-        {
-          title: 'Meeting',
-          start: '2023-01-13T11:00:00',
-          constraint: 'availableForMeeting', // defined below
-          color: '#257e4a'
-        },
-        {
-          title: 'Conference',
-          start: '2023-01-18',
-          end: '2023-01-20'
-        },
-        {
-          title: 'Party',
-          start: '2023-01-29T20:00:00'
-        },
-
-        // areas where "Meeting" must be dropped
-        {
-          groupId: 'availableForMeeting',
-          start: '2023-01-11T10:00:00',
-          end: '2023-01-11T16:00:00',
-          display: 'background'
-        },
-        {
-          groupId: 'availableForMeeting',
-          start: '2023-01-13T10:00:00',
-          end: '2023-01-13T16:00:00',
-          display: 'background'
-        },
-
-        // red areas where no events can be dropped
-        {
-          start: '2023-01-24',
-          end: '2023-01-28',
-          overlap: false,
-          display: 'background',
-          color: '#ff9f89'
-        },
-        {
-          start: '2023-01-06',
-          end: '2023-01-08',
-          overlap: false,
-          display: 'background',
-          color: '#ff9f89'
-        }
-      ]
-    });
-
-    calendar.render();
-  });
-
-</script>
-<style>
-
-  body {
-    margin: 40px 10px;
-    padding: 0;
-    font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
-    font-size: 14px;
-  }
-
-  #calendar {
-    max-width: 1100px;
-    margin: 0 auto;
-  }
-
-</style>
-</head>
-<body>
-
-  <div id='calendar'></div>
-
-</body>
-</html> -->
